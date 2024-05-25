48ce64df2b8e47b47d13d77f747199ac41a877d1
tomegoodson <158308016+tomegoodson@users.noreply.github.com>
2024-05-25 10:40:27 -0500

    deletebutton.tg

---
+++ Components/Delete.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DeleteButton = ({ onDelete }) => (
+  <button type="button" onClick={onDelete}>Delete</button>
+);
+
+export default DeleteButton;
--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import {BrowserRouter as Router, Route, Switch } from 'react-router';
+import NavBar from './components/NavBar';
+import MovieList from './components/MovieList';
+import MovieDetail from './components/MovieDetail';
+import AddMovie from './components/AddMovie';
+import GenreFilter from './components/GenreFilter';
+import './app.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = ()
 
-export default App;
+
